d41f1a5809600528e09565952e1fbd7d8a77e7ee
Zack Siri <me@zacksiri.dev>
2024-07-02 23:24:06 +0700

    update output for test that are skipped

--- test/icepak/checks_test.exs
+++ test/icepak/checks_test.exs
@@ -172,5 +172,89 @@ defmodule Icepak.ChecksTest do
 
       assert event["name"] == "activate"
     end
+
+    test "skip passed assessment", %{
+      os: os,
+      checks: checks,
+      release: release,
+      arch: arch,
+      variant: variant,
+      serial: serial,
+      path: path
+    } do
+      Icepak.PolarMock
+      |> expect(:authenticate, fn -> Req.new(base_url: "http://localhost:4000") end)
+
+      Icepak.PolarMock
+      |> expect(:get_product, fn _client, _product_key ->
+        %{
+          status: 200,
+          body: %{
+            "data" => %{
+              "id" => 1,
+              "key" => "some-key",
+              "requirements" => %{"secureboot" => "false"}
+            }
+          }
+        }
+      end)
+
+      Icepak.PolarMock
+      |> expect(:get_version, fn _client, _product, _serial ->
+        %{status: 200, body: %{"data" => %{"id" => 1}}}
+      end)
+
+      Icepak.PolarMock
+      |> expect(:transition_version, fn _client, _version, event ->
+        assert event["name"] == "test"
+
+        %{status: 201, body: %{"data" => %{"id" => 1, "name" => "test"}}}
+      end)
+
+      Icepak.PolarMock
+      |> expect(:get_testing_clusters, fn _client ->
+        [
+          %Cluster{
+            id: 1,
+            type: "lxd",
+            arch: "amd64",
+            current_state: "healthy",
+            endpoint: "https://localhost:8443",
+            private_key: "some-key",
+            certificate: "some-cert"
+          }
+        ]
+      end)
+
+      Icepak.PolarMock
+      |> expect(:get_testing_checks, fn _client ->
+        [
+          %Check{id: 1, name: "ipv4"}
+        ]
+      end)
+
+      Icepak.PolarMock
+      |> expect(:get_or_create_testing_assessment, fn _client, _version, _params ->
+        %Req.Response{status: 200, body: %{"data" => %{"id" => 1, "current_state" => "passed"}}}
+      end)
+
+      Icepak.PolarMock
+      |> expect(:transition_version, fn _client, _version, event ->
+        assert event["name"] == "activate"
+
+        %Req.Response{status: 201, body: %{"data" => %{"id" => 1, "name" => "activate"}}}
+      end)
+
+      assert %{status: 201, body: %{"data" => _event}} =
+               Icepak.Checks.perform(
+                 os: os,
+                 checks: checks,
+                 release: release,
+                 arch: arch,
+                 variant: variant,
+                 serial: serial,
+                 path: path
+               )
+    end
   end
 end
